0fe8a311a31d17ca5a2a666ac78c8dda69a7764c
flamemode <flemmingravndal@hotmail.com>
2025-04-17 14:09:17 +0200

    first major facelift for tjuvpakk

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <img src="/images/logo.png" alt="Tjuvpakk-logo" className="logo-image" />
     <App />
   </React.StrictMode>,
 );
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}', // Ensure all JS/JSX/TS/TSX files are scanned
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
