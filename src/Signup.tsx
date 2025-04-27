cee70ef8345a936b129cedc8f5d86728df2fb4f3
mikaelra <mikaelravndal@gmail.com>
2025-04-27 23:38:05 +0200

    Ts igjen

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BACKEND_URL } from "./config";
 
 export default function Signup() {
