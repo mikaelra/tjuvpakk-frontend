1d2e5f447eeb026a78a7378a201a9174196b17dd
mikaelra <mikaelravndal@gmail.com>
2025-08-05 14:20:18 +0200

    Was in main, back to standard backend url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-//export const BACKEND_URL = "https://tjuvpakk-backend.onrender.com"; //ONLINE
-export const BACKEND_URL = "https://tjuvpakk-backend-pr-32.onrender.com/";
+export const BACKEND_URL = "https://tjuvpakk-backend.onrender.com"; //ONLINE
+//export const BACKEND_URL = "http://localhost:5000";
